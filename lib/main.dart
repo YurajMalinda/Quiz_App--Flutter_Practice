7213a5b301c392cb9f0ff190eed4fe83688c38e0
YurajMalinda <yurajmalinda123.com>
2024-06-26 20:46:10 +0530

    start screen completed.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:adv_basics/start_screen.dart';
 
+const startAlignment = Alignment.topCenter;
+const endAlignment = Alignment.bottomCenter;
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: StartScreen(
-          Color.fromARGB(255, 77, 0, 145),
-          Color.fromARGB(255, 137, 0, 235),
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 77, 0, 145),
+                Color.fromARGB(255, 137, 0, 235),
+              ],
+              begin: startAlignment,
+              end: endAlignment,
+            ),
+          ),
+          child: const StartScreen(),
         ),
       ),
     ),
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:adv_basics/question_img.dart';
-
-const startAlignment = Alignment.topCenter;
-const endAlignment = Alignment.bottomCenter;
 
 class StartScreen extends StatelessWidget {
-  const StartScreen(this.color1, this.color2, {super.key});
-
-  final Color color1;
-  final Color color2;
+  const StartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [color1, color2],
-          begin: startAlignment,
-          end: endAlignment,
-        ),
-      ),
-      child: const Center(
-        child: QuestionImg(),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(134, 255, 255, 255),
+          ),
+          const SizedBox(
+            height: 50,
+          ),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 20,
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text(
+              'Start Quiz',
+            ),
+          )
+        ],
       ),
     );
   }
